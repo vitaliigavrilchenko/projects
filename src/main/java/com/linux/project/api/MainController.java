773a7bfc74a071535714c24a7614843657e114e8
Vitalii Gavrilchenko <vitaliigavrilchenko@gmail.com>
2019-05-30 22:29:51 +0300

    spring boot Controller mapping

--- src/main/java/com/linux/project/api/MainController.java
+++ src/main/java/com/linux/project/api/MainController.java
@@ -1,13 +1,24 @@
 package com.linux.project.api;
 
+import com.linux.project.services.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/hello")
-    public String showHello(){
-        return "hello";
+    public BookService bookService = new BookService();
+
+    @RequestMapping("/name")
+    @ResponseBody
+    public String bookName() {
+        return bookService.findById(20).getName();
+    }
+
+    @RequestMapping("/colour")
+    @ResponseBody
+    public String bookColour() {
+        return bookService.findById(10).getColour();
     }
 }
